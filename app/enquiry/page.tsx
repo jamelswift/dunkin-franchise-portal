9062bdab88780ea032015cffeeb43190fb7c2759
Jamelswift <Wipatsasicha0702@gmail.com>
2026-09-03 10:15:23 +0700

    first commit

---
+++ app/enquiry/page.tsx
@@ -0,0 +1,5 @@
+import { EnquiryPage } from '@/components/enquiry-page'
+
+export default function Page() {
+  return <EnquiryPage />
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import { FranchiseSite } from '@/components/franchise-site'
+
+export default function Page() {
+  return <FranchiseSite />
+}
